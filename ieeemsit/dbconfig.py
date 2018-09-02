87f6dc70eec9bedc6a2699d07c154f886758bf66
sahilkhurana19 <sahil_khurana@outlook.com>
2018-09-02 19:17:36 +0530

    Adding config files

---
+++ ieeemsit/dbconfig.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+
+database_config = {
+    'NAME' : 'ieeemsit_ieeemsit',
+    'USER' : 'sahilkhurana',
+    'PASSWORD': 'psqlpassword'
+}
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+static_root_path = os.path.join(BASE_DIR, 'static-root')
+
+media_root_path = os.path.join(BASE_DIR, 'uploads')
--- ieeemsit/settings.py
+++ ieeemsit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from . import dbconfig 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','erpj6cj)x%^-!xs!d3$8%7*td5xletgmmctn#d1@rvg07b5)6*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '*', 'ieeemsit.a2hosted.com']
 
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -94,9 +94,9 @@ WSGI_APPLICATION = 'ieeemsit.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'ieeemsit_ieeemsit',
-       'USER': 'sahilkhurana',
-       'PASSWORD': 'psqlpassword',
+       'NAME': dbconfig.database_config['NAME'],#'ieeemsit_ieeemsit',
+       'USER': dbconfig.database_config['USER'],#'sahilkhurana',
+       'PASSWORD': dbconfig.database_config['PASSWORD'],#'psqlpassword',
        'HOST': 'localhost',
        'PORT': 5432,
 }
@@ -145,12 +145,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static-root/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
-STATIC_ROOT = '/home/ieeemsit/testdirectory/static-root/'
+STATIC_ROOT = dbconfig.static_root_path#os.path.join(BASE_DIR, 'static-root')
+# STATIC_ROOT = '/home/ieeemsit/testdirectory/static-root/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static-root')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = dbconfig.media_root_path#os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = ""
 CKEDITOR_BROWSE_SHOW_DIRS = True
