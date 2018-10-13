11217f3cd399b9bc195dd9a048f6479fa0f1cfdc
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2018-10-14 02:03:11 +0530

    Update dbconfig.py

--- ieeemsit/dbconfig.py
+++ ieeemsit/dbconfig.py
@@ -19,12 +19,6 @@ database_config = {
 
 
 
-#database_config = {
-#    'NAME' : 'ieeemsit2',
-#    'USER' : 'ankush2',
-#    'PASSWORD': 'garg123ankush'
-#}
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
