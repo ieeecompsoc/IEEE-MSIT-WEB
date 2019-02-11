234ea433669e180d131da56b4b89d95fe5c99f8c
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-02-12 01:31:36 +0530

    omg

--- ieeemsit/dbconfig.py
+++ ieeemsit/dbconfig.py
@@ -5,7 +5,19 @@ database_config = {
     'NAME' : 'ieeemsit_ieeemsit',
     'USER' : 'ieeemsit',
     'PASSWORD': 'psqlpassword'
-}
+# database_config = {
+#     'NAME' : 'ieeemsit_ieeemsit',
+#     'USER' : 'sahilkhurana',
+#     'PASSWORD': 'psqlpassword'
+# }
+
+#database_config = {
+    #'NAME' : 'ieeemsit2',
+    #'USER' : 'ankush2',
+    #'PASSWORD': 'garg123ankush'
+#}
+
+
 
 #database_config = {
 #    'NAME' : 'ieeemsit2',
@@ -20,4 +32,8 @@ static_root_path = os.path.join(BASE_DIR, 'static-root')
 
 media_root_path = os.path.join(BASE_DIR, 'uploads')
 
+<<<<<<< 275c76a63ba1290ff064e20f92267a0fd69da34d
 Debug = False
+=======
+Debug = True
+>>>>>>> ta added
