f80a807ed81e18079bf0d2255868e415beb9c16c
niteshks247 <niteshks247@gmail.com>
2021-12-18 21:45:43 +0530

    Added PES Section, carousel updated

--- ieeemsit/settings.py
+++ ieeemsit/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['localhost', '*', 'ieeemsit.a2hosted.com', '139.59.79.194']
 INSTALLED_APPS = [
     'web',
     'wiemain',
+    'pes_msit',
     'attendance',
     'urlshortner',
     'crispy_forms',
